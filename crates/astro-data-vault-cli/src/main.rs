6b56e03defa26d250eca5fdd87e8f0befca570ce
finiscontiral <193557840+finiscontiral@users.noreply.github.com>
2026-08-13 09:05:31 +0900

    feat: add CLI parser using clap

--- crates/astro-data-vault-cli/src/main.rs
+++ crates/astro-data-vault-cli/src/main.rs
@@ -1,3 +1,27 @@
+use clap::{Parser, ValueEnum};
+
+/// AstroDataVault CLI
+#[derive(Debug, Parser)]
+#[command(version, about)]
+struct Cli {
+    /// File path
+    file_path: String,
+
+    /// Data format
+    #[arg(long, short, value_enum, default_value_t = OdmFormat::Tle, ignore_case = true)]
+    odm_fmt: OdmFormat,
+}
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, ValueEnum)]
+#[value(rename_all = "upper")]
+enum OdmFormat {
+    Tle,
+    Omm,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    println!("File Path: {}", cli.file_path);
+    println!("ODM Format: {:?}", cli.odm_fmt);
 }
